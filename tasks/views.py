bd7b6e537887f69cb294ed9058f425a0b47c2665
manavdesai27 <manav27202@gmail.com>
2021-03-04 14:38:58 +0530

    Django 1.3

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
-
+tasks = ["ID-CARD", "FOOD", "SLEEP"]
 # Create your views here.
 
 def index(request):
-    
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
